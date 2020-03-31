ed23196367ae678c5f8471bfc688d10462e94607
ThomasFeldman <tfeldman1.31@gmail.com>
2020-03-31 15:16:46 -0500

    added rest of functionality from cheese-mvc, still using that to learn from

--- src/main/java/org/launchcode/pao/Controllers/PaoController.java
+++ src/main/java/org/launchcode/pao/Controllers/PaoController.java
@@ -1,18 +1,26 @@
 package org.launchcode.pao.Controllers;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("cheese")
 public class PaoController {
-    static ArrayList<String> cheeses = new ArrayList<>();
 
+    static HashMap<String, String> cheeses = new HashMap<>();
+
+    //Request path: /cheese
     @RequestMapping(value = "")
     public String index(Model model) {
+
         model.addAttribute("cheeses", cheeses);
         model.addAttribute("title", "My Cheeses");
+
         return "index";
     }
 
@@ -23,8 +31,26 @@ public class PaoController {
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheese(@RequestParam String cheeseName) {
-        cheeses.add(cheeseName);
+    public String processAddCheese(@RequestParam String cheeseName,
+       @RequestParam String cheeseDescription) {
+        cheeses.put(cheeseName, cheeseDescription);
+        return "redirect:";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.GET)
+    public String displayRemoveCheeseForm(Model model) {
+        model.addAttribute("cheeses", cheeses.keySet());
+        model.addAttribute("title", "Remove Cheese");
+        return "remove";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.POST)
+    public String processRemoveCheeseForm(@RequestParam ArrayList<String> cheese) {
+
+        for (String aCheese : cheese) {
+            cheeses.remove(aCheese);
+        }
+
         return "redirect:";
     }
 
