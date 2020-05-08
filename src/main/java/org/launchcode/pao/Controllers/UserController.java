45078e5e8a0cf72cec1d87c03925b773ebb56b4e
ThomasFeldman <tfeldman1.31@gmail.com>
2020-05-07 23:08:13 -0500

    not sure if Taglibs is the way to go

--- src/main/java/org/launchcode/pao/Controllers/UserController.java
+++ src/main/java/org/launchcode/pao/Controllers/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.taglibs.TagLibConfig;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
